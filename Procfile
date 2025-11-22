2d7aa02dcd33de04594d98b588eff0d2dd86f64e
Abhi <abhaychinu123@gmail.com>
2025-11-22 11:34:55 +0530

    Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn apps.routes.socket:socketio --worker-class eventlet -w 1 --bind 0.0.0.0:5000
--- app.py
+++ app.py
@@ -18,12 +18,16 @@ if __name__ == '__main__':
     # 2. Run the application with SocketIO
     # Note: Using socketio.run instead of app.run
     # Set FLASK_DEBUG=True in .env for development
-    socketio.run(
-        app,
-        host="0.0.0.0",
-        debug=os.environ.get('FLASK_DEBUG', 'True') == 'True',
-        port=5000,
-        allow_unsafe_werkzeug=True # Needed for some dev setups
-    )
+    # socketio.run(
+    #     app,
+    #     host="0.0.0.0",
+    #     debug=os.environ.get('FLASK_DEBUG', 'True') == 'True',
+    #     port=5000,
+    #     allow_unsafe_werkzeug=True # Needed for some dev setups
+    # )
+    
+    if __name__ == '__main__':
+        socketio.run(app, host="0.0.0.0", port=5000)
+
     
     
